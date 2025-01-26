3bdcf32e4b15d8e6931dd950b4dab86bd6392d78
AnoopKumar <anoopkumarsingh678@gmail.com>
2025-01-26 16:30:41 +0100

    generate HTML for the order Summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];
+export const cart = [{
+  productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity: 2,
+},{
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity: 1
+}];
+
+
 
 
 
@@ -7,7 +15,7 @@ export function addToCart(productId){//add a product to the cart
   let matchingItem;
 
   cart.forEach((cartItem)=>{
-  if(productId ===cartItem.productId){
+  if(productId === cartItem.productId){
     matchingItem = cartItem;
   }
   });
